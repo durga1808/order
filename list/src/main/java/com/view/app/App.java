63992cd7eb0b392031146a5fa22c5d5b95984c1f
durga1808 <durgalakshmi3713@gmail.com>
2023-07-18 10:10:25 +0530

    inserted

---
+++ list/src/main/java/com/view/app/App.java
@@ -0,0 +1,13 @@
+package com.view.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
