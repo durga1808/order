ffe823dbd2df5bda6bfa50858671e1d891f2aa8a
hariharan <hariduke001@gmail.com>
2023-09-01 17:16:47 +0530

    Theme templated updated

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { ColorModeContext, useMode } from "./theme";
+import { Route, Routes } from "react-router";
+import Topbar from "./global/Topbar";
 
 function App() {
+  const [theme, colorMode] = useMode();
   return (
-    <div className="App">
-      <header className="App-header">
-        HELLO WORLD!
-      </header>
-    </div>
+    <ColorModeContext.Provider value={colorMode}>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Routes>
+            <Route path="/" element={<Topbar />} />
+          </Routes>
+        </ThemeProvider>
+      </ColorModeContext.Provider>
   );
 }
 
