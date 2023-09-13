edacd4b524f73254077e81d2515bfa16a9e21dbd
Raga2707 <ragamalya295@gmail.com>
2023-09-13 17:06:11 +0530

    Change card styles in traceslist

--- src/scenes/traces/trace/TraceList.jsx
+++ src/scenes/traces/trace/TraceList.jsx
@@ -10,52 +10,58 @@ import { tokens } from '../../../theme';
 
 const mockTraces = [
     {
-        servicename: 'Service 1',
-        endPoint: '/endpoint1',
+        servicename: 'orderService',
+        endPoint: '/order/getOrderById/{id}',
         traceID: '123456987667898767898768987984594b345679',
-        duration: 'a few seconds ago',
+        createdTime: 'a few seconds ago',
         statusCode: '200',
         method: 'GET',
+        duration: '100 ms',
     },
     {
         servicename: 'Service 2',
         endPoint: '/endpoint2',
         traceID: '789012',
-        duration: '300 ms',
+        createdTime: '1 min ago',
         statusCode: '404',
         method: 'POST',
+        duration: '200 ms',
     },
     {
         servicename: 'Service 3',
         endPoint: '/endpoint3',
         traceID: '345678',
-        duration: '200 ms',
+        createdTime: '2 min ago',
         statusCode: '200',
         method: 'GET',
+        duration: '250 ms',
     },
     {
         servicename: 'Service 4',
         endPoint: '/endpoint4',
         traceID: '123456',
-        duration: '100 ms',
+        createdTime: '5 min ago',
         statusCode: '200',
         method: 'GET',
+        duration: '200 ms',
     },
     {
         servicename: 'Service 5',
         endPoint: '/endpoint5',
         traceID: '123456',
-        duration: '100 ms',
+        createdTime: '10 min ago',
         statusCode: '200',
         method: 'GET',
+        duration: '150 ms',
     },
     {
         servicename: 'Service 6',
         endPoint: '/endpoint6',
         traceID: '123456',
-        duration: '100 ms',
+        createdTime: '15 min ago',
         statusCode: '200',
         method: 'GET',
+        duration: '200 ms',
     }
 ];
 
@@ -74,8 +80,8 @@ const TraceList = () => {
 
     return (
         <div style={{ maxHeight: "calc(100vh - 50px)", overflowY: "auto" }} >
-            <div className="container"  >
-                <Box width="100%" display="flex" flexDirection="row" justifyContent="space-between" margin="10px 10px 0 10px" >
+            <div className="container" >
+                <Box width="100%" display="flex" flexDirection="row" justifyContent="space-between" margin="0 10px 0 10px" >
                     <Typography variant="h6" style={{ margin: "10px 0 20px 10px" }}>Traces ({mockTraces.length})</Typography>
 
                     <FormControl sx={{ width: "40%" }}>
@@ -102,7 +108,10 @@ const TraceList = () => {
                 {mockTraces.map((trace, index) => (
                     <Card className="tracelist-card" key={index} sx={{ margin: "10px 0 20px 0", width: "500px", height: "fit-content" }} >
                         <CardActionArea>
-                            <CardHeader title={<Typography variant="h6" sx={{ backgroundColor: colors.greenAccent[500], paddingInlineStart: "10px" }}>{trace.servicename}: {trace.endPoint}</Typography>} />
+                            <CardHeader title={<Typography variant="h6" sx={{ backgroundColor: colors.greenAccent[500], paddingInlineStart: "10px", display: "flex", justifyContent: "space-between" }}>
+                                <span>{trace.servicename}: {trace.endPoint}</span>
+                                <span>{trace.duration}</span>
+                            </Typography>} />
                             <CardContent style={{ marginTop: "-20px"}}>
                                     {/* orderProject: /get/getAllOrder
                             <br />
@@ -118,7 +127,7 @@ const TraceList = () => {
                                     </Typography>
 
                                     <Typography variant="h8" style={{ display: "flex", justifyContent: "space-between", alignItems: "flex-end", margin: "15px 0 0 0 " }}>
-                                        <span>{trace.duration}</span>
+                                        <span>{trace.createdTime}</span>
                                         <span>StatusCode: {trace.statusCode}</span>
                                         <span>Method: {trace.method}</span>
                                     </Typography>
