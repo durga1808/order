092ff05a0a82485d20dedbb99b9155acae2af461
hariharan <hariduke001@gmail.com>
2023-09-14 09:30:28 +0530

    Pages Added for React Span Flow

--- src/scenes/traces/index.jsx
+++ src/scenes/traces/index.jsx
@@ -2,22 +2,28 @@ import React from "react";
 import TraceList from "./trace/TraceList";
 import { Box, Card, useTheme } from "@mui/material";
 import { tokens } from "../../theme";
+import SpanFlow from "./trace/SpanFlow";
 
 const Traces = () => {
   const theme = useTheme();
   const colors = tokens(theme.palette.mode);
   return (
-    <div >
+    <div style={{ display: "flex", flexDirection: "row", justifyContent: "normal" }} >
       {/* <TraceTopBar /> */}
-      <Box sx={{width:"550px", m: "40px 20px 20px 20px" }} >
+      <Box sx={{ width: "550px", m: "40px 20px 20px 20px" }} >
         <Card sx={{ backgroundColor: colors.primary[400], padding: "15px" }}>
           <TraceList />
         </Card>
       </Box>
 
       <div>
-
+        <Box sx={{m: "40px 20px 20px 10px", width:"650px", }} >
+          <Card sx={{ backgroundColor: colors.primary[400], padding: "15px" }}>
+            <SpanFlow />
+          </Card>
+        </Box>
       </div>
+
 
     </div >
   );
---
+++ src/scenes/traces/trace/SpanFlow.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const SpanFlow = () => {
+  return (
+    <div>SpanFlow</div>
+  )
+}
+
+export default SpanFlow
