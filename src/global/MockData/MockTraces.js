d135a4eca2ca18d4ee98bd3ee9b4d1c14c421092
hariharan <hariduke001@gmail.com>
2023-09-29 09:44:38 +0530

    Trace section api integration

--- src/global/MockData/MockTraces.js
+++ src/global/MockData/MockTraces.js
@@ -43,24 +43,59 @@ const mockTraces = [
 
 const TraceSummaryMock = [
   {
-      serviceName: "order-project",
-      totalApiCalls: 70,
-      peakLatency: "3000ms",
-      totalErrorCalls: 40,
-      totalSuccessCalls: 30
+    serviceName: "order-project",
+    totalApiCalls: 70,
+    peakLatency: "3000ms",
+    totalErrorCalls: 40,
+    totalSuccessCalls: 30
   },
   {
-      serviceName: "vendor-project",
-      totalApiCalls: 50,
-      peakLatency: "5000ms",
-      totalErrorCalls: 20,
-      totalSuccessCalls: 30
+    serviceName: "vendor-project",
+    totalApiCalls: 50,
+    peakLatency: "5000ms",
+    totalErrorCalls: 20,
+    totalSuccessCalls: 30
   },
   {
-      serviceName: "personal-details",
-      totalApiCalls: 30,
-      peakLatency: "2000ms",
-      totalErrorCalls: 10,
-      totalSuccessCalls: 20
+    serviceName: "personal-details",
+    totalApiCalls: 30,
+    peakLatency: "2000ms",
+    totalErrorCalls: 10,
+    totalSuccessCalls: 20
   }
-]
+];
+
+export const options = [
+  {
+    "value": 30,
+    "label": "30 minutes"
+  },
+  {
+    "value": 60,
+    "label": "1 hour"
+  },
+  {
+    "value": 120,
+    "label": "2 hours"
+  },
+  {
+    "value": 240,
+    "label": "4 hours"
+  },
+  {
+    "value": 480,
+    "label": "8 hours"
+  },
+  {
+    "value": 720,
+    "label": "12 hours"
+  },
+  {
+    "value": 960,
+    "label": "16 hours"
+  },
+  {
+    "value": 1440,
+    "label": "24 hours"
+  }
+];
