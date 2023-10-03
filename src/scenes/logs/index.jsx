807bcc2281e77c1d3fa98a75f02fc9f7050be062
hariharan <hariduke001@gmail.com>
2023-10-03 15:31:25 +0530

    Added loading for trace and integrated trace to log api

--- src/scenes/logs/index.jsx
+++ src/scenes/logs/index.jsx
@@ -1,6 +1,14 @@
-import React from 'react'
+import React, { useContext, useEffect } from 'react'
+import { GlobalContext } from '../../global/globalContext/GlobalContext'
 
 const Logs = () => {
+
+    const { logData, traceToLogError } = useContext(GlobalContext);
+
+    useEffect(() => {
+        console.log("LogData " + logData + "  " + traceToLogError);
+    }, [logData, traceToLogError])
+
     return (
         <div>Logs</div>
     )
