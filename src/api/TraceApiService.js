43603d72f221412f07524e1836f4c6ac0c409547
mariselvamchellaiya <mariselvamchellaiya@gmail.com>
2023-11-03 15:43:25 +0530

    PeakLatency filter style and api Integration done

--- src/api/TraceApiService.js
+++ src/api/TraceApiService.js
@@ -103,7 +103,6 @@ export const TraceFilterOptionWithDate = async (
     if (JSON.parse(localStorage.getItem("needHistoricalData"))) {
       console.log(
         `History call + ${traceURL}/TraceQueryFilter?from=${endDate}&page=${page}&pageSize=${pageSize}&sortOrder=${sortorder}&to=${startDate}`
-
       );
       finalUrl = `${traceURL}/TraceQueryFilter?from=${endDate}&page=${page}&pageSize=${pageSize}&sortOrder=${sortorder}&to=${startDate}`;
     } else {
@@ -139,13 +138,15 @@ export const FindByTraceIdForSpans = async (traceId) => {
 
 export const findLogByErrorTrace = async (traceId) => {
   try {
-    const response = await axios.get(`${traceURL}/getByErrorTraceId?traceId=${traceId}`);
+    const response = await axios.get(
+      `${traceURL}/getByErrorTraceId?traceId=${traceId}`
+    );
     return response.data;
   } catch (error) {
     console.error("Error retrieving users:", error);
     return error;
   }
-}
+};
 
 export const getTraceSummaryData = async (timeMinutesAgo) => {
   try {
@@ -219,12 +220,6 @@ export const getRecentTraceList = async (page, pageSize, serviceName) => {
   }
 };
 
-
-
-
-
-
-
 export const getDbSummaryDataWithDate = async (
   startDate,
   endDate,
@@ -261,30 +256,62 @@ export const getDbSummaryDataWithDate = async (
   }
 };
 
-
 export const getKafkaSummaryData = async (startDate, endDate, minutesAgo) => {
-    try {
-      const serviceListData = JSON.parse(localStorage.getItem("serviceListData"));
-      const serviceNameListParam = serviceListData.join("&serviceNameList=");
-  
-      var finalUrl;
-  
-      if (JSON.parse(localStorage.getItem("needHistoricalData"))) {
-        console.log(
-          `History call + ${traceURL}/KafkaSumaryChartDataCount?from=${startDate}&serviceNameList=${serviceNameListParam}&to=${endDate}`
-        );
-        finalUrl = `${traceURL}/KafkaSumaryChartDataCount?from=${startDate}&serviceNameList=${serviceNameListParam}&to=${endDate}`;
-      } else {
-        console.log(
-          `Minutes call + ${traceURL}/KafkaSumaryChartDataCount?minutesAgo=${minutesAgo}&serviceNameList=${serviceNameListParam}&to=${startDate}`
-        );
-        finalUrl = `${traceURL}/KafkaSumaryChartDataCount?from=${startDate}&minutesAgo=${minutesAgo}&serviceNameList=${serviceNameListParam}`;
-      }
-  
-      const response = await axios.get(finalUrl);
-      return response.data;
-    } catch (error) {
-      console.error("Error retrieving users:", error);
-      throw error;
+  try {
+    const serviceListData = JSON.parse(localStorage.getItem("serviceListData"));
+    const serviceNameListParam = serviceListData.join("&serviceNameList=");
+
+    var finalUrl;
+
+    if (JSON.parse(localStorage.getItem("needHistoricalData"))) {
+      console.log(
+        `History call + ${traceURL}/KafkaSumaryChartDataCount?from=${startDate}&serviceNameList=${serviceNameListParam}&to=${endDate}`
+      );
+      finalUrl = `${traceURL}/KafkaSumaryChartDataCount?from=${startDate}&serviceNameList=${serviceNameListParam}&to=${endDate}`;
+    } else {
+      console.log(
+        `Minutes call + ${traceURL}/KafkaSumaryChartDataCount?minutesAgo=${minutesAgo}&serviceNameList=${serviceNameListParam}&to=${startDate}`
+      );
+      finalUrl = `${traceURL}/KafkaSumaryChartDataCount?from=${startDate}&minutesAgo=${minutesAgo}&serviceNameList=${serviceNameListParam}`;
     }
+
+    const response = await axios.get(finalUrl);
+    return response.data;
+  } catch (error) {
+    console.error("Error retrieving users:", error);
+    throw error;
   }
+};
+
+export const getPeakLatencyFilterData = async (
+  startDate,
+  PeakLatency,
+  endDate,
+  minutesAgo,
+ 
+) => {
+  try {
+    const serviceListData = JSON.parse(localStorage.getItem("serviceListData"));
+    const serviceNameListParam = serviceListData.join("&serviceNameList=");
+
+    var finalUrl;
+
+    if (JSON.parse(localStorage.getItem("needHistoricalData"))) {
+      console.log(
+        `History call + ${traceURL}/TraceSumaryChartPeaKLatencyCount?from=${endDate}&peakLatencyThreshold=${PeakLatency}&serviceNameList=${serviceNameListParam}&to=${startDate}`
+      );
+      finalUrl = ` ${traceURL}/TraceSumaryChartPeaKLatencyCount?from=${endDate}&peakLatencyThreshold=${PeakLatency}&serviceNameList=${serviceNameListParam}&to=${startDate}`;
+    } else {
+      console.log(
+        `Minutes call + ${traceURL}/TraceSumaryChartPeaKLatencyCount?minutesAgo=${minutesAgo}&peakLatencyThreshold=${PeakLatency}&serviceNameList=${serviceNameListParam}&to=${startDate}`
+      );
+      finalUrl = `${traceURL}/TraceSumaryChartPeaKLatencyCount?minutesAgo=${minutesAgo}&peakLatencyThreshold=${PeakLatency}&serviceNameList=${serviceNameListParam}&to=${startDate}`;
+    }
+
+    const response = await axios.get(finalUrl);
+    return response.data;
+  } catch (error) {
+    console.error("Error retrieving users:", error);
+    throw error;
+  }
+};
--- src/scenes/dashboard/summary/TraceCharts/PeakLatencyChart.js
+++ src/scenes/dashboard/summary/TraceCharts/PeakLatencyChart.js
@@ -1,83 +1,179 @@
-import React from "react";
-import ReactApexChart from "react-apexcharts";
-import { tokens } from "../../../../theme";
-import { useTheme } from "@emotion/react";
-import { useContext } from "react";
-import { GlobalContext } from "../../../../global/globalContext/GlobalContext";
-import "./PeakLatencyChart.css";
+  import React, { useState } from "react";
+  import ReactApexChart from "react-apexcharts";
+  import { tokens } from "../../../../theme";
+  import { useTheme } from "@emotion/react";
+  import { useContext } from "react";
+  import { GlobalContext } from "../../../../global/globalContext/GlobalContext";
+  import "./PeakLatencyChart.css";
+  import { CircularProgress, MenuItem, Select, Typography } from "@mui/material";
+  import { useEffect } from "react";
+  import { getPeakLatencyFilterData } from "../../../../api/TraceApiService";
+  import { useCallback } from "react";
+  import Loading from "../../../../global/Loading/Loading";
 
-const PeakLatencyChart = ({ data }) => {
-  const theme = useTheme();
-  const colors = tokens(theme.palette.mode);
-  const { isCollapsed } = useContext(GlobalContext);
+  const PeakLatencyChart = () => {
+    const {
+      lookBackVal,
+      setActiveTab,
+      setSelected,
+      selectedStartDate,
+      selectedEndDate,
+      needHistoricalData,
+      setNavActiveTab,
+    } = useContext(GlobalContext);
 
+    const [errorMessage, setErrorMessage] = useState("");
+    const [emptyMessage, setEmptyMessage] = useState("");
+    const [loading, setLoading] = useState(false);
+    const [peaklatencyData, setPeakLatencyData] = useState([]);
+    const [selectedOption, setSelectedOption] = useState("500");
 
-  const peakLatencyInput = () => {
-    return (
-      <h1>
-        PeakLatency Value
-      </h1>
-    )
-  }
+    // const [FiteredData,setFiteredData] = useState([]);
 
-  const peakLatencyOptions = {
-    chart: {
-      height: 250,
-      type: "bar",
-      toolbar: {
-        show: true,
-        offsetX: -2,
-        offsetY: -25
+    const PeaklatencyFiltered = useCallback(async () => {
+      try {
+        setLoading(true);
+        var response = await getPeakLatencyFilterData(
+          selectedStartDate,
+          selectedOption,
+          selectedEndDate,
+          lookBackVal.value
+        );
+        console.log("Peakaltency data " + response);
+        if (response.length !== 0) {
+          setPeakLatencyData(response);
+        } else {
+          setEmptyMessage("No Data to show");
+        }
+        setLoading(false);
+      } catch (error) {
+        console.log("ERROR on peaklatency filter api " + error);
+        setErrorMessage("An error Occurred!");
+        setLoading(false);
       }
-    },
-    plotOptions: {
-      bar: {
-        columnWidth: "30px",
-      },
-    },
+    }, [
+      selectedStartDate,
+      selectedEndDate,
+      lookBackVal,
+      selectedOption,
+      needHistoricalData,
+    ]);
 
-    xaxis: {
-      labels: {
-        rotate: -45,
-        style: {
-          colors: colors.textColor[500],
-          fontSize: 10,
-          fontWeight: 500,
-          fontFamily: "Red Hat Display",
+    useEffect(() => {
+      setErrorMessage("");
+      setEmptyMessage("");
+      PeaklatencyFiltered();
+      setActiveTab(3);
+      setNavActiveTab(0);
+    }, [
+      setErrorMessage,
+      setEmptyMessage,
+      PeaklatencyFiltered,
+      setActiveTab,
+      setNavActiveTab,
+    ]);
+
+    const theme = useTheme();
+    const colors = tokens(theme.palette.mode);
+    const { isCollapsed } = useContext(GlobalContext);
+
+    const handleSortOrderChange = (event) => {
+      setSelectedOption(event.target.value);
+      // setCurrentPage(1)
+    };
+
+    const peakLatencyInput = () => {
+      return <h1>PeakLatency Value</h1>;
+    };
+
+    const sortOrderOptions = [
+      {
+        label: "50",
+        value: "50",
+      },
+      {
+        label: "100",
+        value: "100",
+      },
+      {
+        label: "200",
+        value: "200",
+      },
+      {
+        label: "500",
+        value: "500",
+      },
+      {
+        label: "700",
+        value: "700",
+      },
+      {
+        label: "1000",
+        value: "1000",
+      },
+      {
+        label: "> 2000",
+        value: "2000",
+      },
+    ];
+    const peakLatencyOptions = {
+      chart: {
+        height: 250,
+        type: "bar",
+        toolbar: {
+          show: true,
+          offsetX: -2,
+          offsetY: -25,
         },
       },
-      categories: data.map((item) => item.serviceName),
-      title: {
-        text: "List of Services",
-        style: {
-          color: colors.textColor[500],
-          fontSize: 12,
-          fontWeight: 500,
-          fontFamily: "Red Hat Display",
-        },
-      },
-    },
-    yaxis: {
-      title: {
-        text: "Latency(ms)",
-        style: {
-          color: colors.textColor[500],
-          fontSize: 12,
-          fontWeight: 500,
-          fontFamily: "Red Hat Display",
+      plotOptions: {
+        bar: {
+          columnWidth: "30px",
         },
       },
 
-      labels: {
-        style: {
-          colors: colors.textColor[500],
-          fontSize: 12,
-          fontWeight: 500,
-          fontFamily: "Red Hat Display",
+      xaxis: {
+        labels: {
+          rotate: -45,
+          style: {
+            colors: colors.textColor[500],
+            fontSize: 10,
+            fontWeight: 500,
+            fontFamily: "Red Hat Display",
+          },
+        },
+        categories: peaklatencyData.map((item) => item.serviceName),
+        title: {
+          text: "List of Services",
+          style: {
+            color: colors.textColor[500],
+            fontSize: 12,
+            fontWeight: 500,
+            fontFamily: "Red Hat Display",
+          },
         },
       },
-    },
-    // title: {
+      yaxis: {
+        title: {
+          text: "Latency(ms)",
+          style: {
+            color: colors.textColor[500],
+            fontSize: 12,
+            fontWeight: 500,
+            fontFamily: "Red Hat Display",
+          },
+        },
+
+        labels: {
+          style: {
+            colors: colors.textColor[500],
+            fontSize: 12,
+            fontWeight: 500,
+            fontFamily: "Red Hat Display",
+          },
+        },
+      },
+      // title: {
       // text: "Peak Latency > 500(ms)",
       // align: "middle",
       // offsetX: 0,
@@ -88,44 +184,120 @@ const PeakLatencyChart = ({ data }) => {
       //   fontWeight: 500,
       //   fontFamily: "Red Hat Display",
       // },
-      
-    // },
-    labels: {
-      style: {
-        colors: theme.palette.mode === "dark" ? "#FFF" : "#000",
+
+      // },
+      labels: {
+        style: {
+          colors: theme.palette.mode === "dark" ? "#FFF" : "#000",
+        },
       },
-    },
-  };
-  const peakLatencySeries = [
-    {
-      name: "Peak Latency",
-      data: data.map((item) => item.peakLatency),
-    },
-  ];
+    };
+    const peakLatencySeries = [
+      {
+        name: "Peak Latency",
+        data: peaklatencyData.map((item) => item.peakLatency),
+      },
+    ];
 
+    const chartWidth = isCollapsed ? "calc(100% - 20px)" : "calc(103% - 30px)";
 
-  const chartWidth = isCollapsed ? 'calc(100% - 20px)' : 'calc(103% - 30px)'
-
-  return (
-    <>
-      <div className="chart-title">
-        <p>Peak Latency &gt; 500(ms)</p>
-      </div>
-
-      <div data-theme={theme.palette.mode} style={{ height: "calc(40vh - 20px)", width: chartWidth,marginTop:"-25px" }} >
-        <ReactApexChart
+    return (
+      <>
+        <div
+          className="chart-title"
           style={{
-            color: "#000",
+            display: "flex",
+            flexDirection: "row",
+            justifyContent: "center",
+            marginBottom: "10px",
+            // marginRight:"-30px"
+            marginLeft: "50px",
           }}
-          options={peakLatencyOptions}
-          series={peakLatencySeries}
-          type="bar"
-          height={"80%"}
-          width={"100%"}
-        />
-      </div>
-    </>
-  );
-};
+        >
+          <p>Peak Latency &gt; {selectedOption}(ms)</p>
 
-export default PeakLatencyChart;
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              // marginBottom: "10px",
+            }}
+          >
+            <label
+              style={{
+                fontSize: "8px",
+                fontWeight:"500",
+                // marginLeft: "20px",
+              }}
+            >
+              Filter (ms)
+            </label>
+
+            <Select
+              value={selectedOption}
+              onChange={handleSortOrderChange}
+              style={{
+                width: "95px",
+                height: "32px",
+                marginLeft: "50px",
+                // borderColor:"red"
+              }}
+            >
+              <MenuItem value="" disabled>
+                Sort Order
+              </MenuItem>
+              {sortOrderOptions.map((option, index) => (
+                <MenuItem key={index} value={option.value}>
+                  {option.label}
+                </MenuItem>
+              ))}
+            </Select>
+          </div>
+        </div>
+
+        <div
+          data-theme={theme.palette.mode}
+          style={{
+            height: "calc(40vh - 20px)",
+            width: chartWidth,
+            marginTop: "-30px",
+          }}
+        >
+          {loading ? (
+            <div
+              style={{
+                display: "flex",
+                flexDirection: "column",
+                justifyContent: "center",
+                alignItems: "center",
+                width: "100%",
+                // height: "80vh",
+              }}
+            >
+              <CircularProgress
+                style={{ color: colors.blueAccent[400], marginTop: "65px" }}
+                size={30}
+                thickness={7}
+              />
+              <Typography variant="h5" fontWeight={"500"} mt={2}>
+                LOADING.....
+              </Typography>
+            </div>
+          ) : (
+            <ReactApexChart
+              style={{
+                color: "#000",
+              }}
+              options={peakLatencyOptions}
+              series={peakLatencySeries}
+              type="bar"
+              height={"80%"}
+              width={"100%"}
+            />
+          )}
+        </div>
+      </>
+    );
+  };
+
+  export default PeakLatencyChart;
