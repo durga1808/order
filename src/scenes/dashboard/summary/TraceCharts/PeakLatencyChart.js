f440351990e250bfb0a7864c94d641c69628f239
hariharan <hariduke001@gmail.com>
2023-11-03 12:11:16 +0530

    Task on peak-latency range

--- src/scenes/dashboard/summary/TraceCharts/PeakLatencyChart.js
+++ src/scenes/dashboard/summary/TraceCharts/PeakLatencyChart.js
@@ -4,16 +4,31 @@ import { tokens } from "../../../../theme";
 import { useTheme } from "@emotion/react";
 import { useContext } from "react";
 import { GlobalContext } from "../../../../global/globalContext/GlobalContext";
+import "./PeakLatencyChart.css";
 
 const PeakLatencyChart = ({ data }) => {
   const theme = useTheme();
   const colors = tokens(theme.palette.mode);
   const { isCollapsed } = useContext(GlobalContext);
 
+
+  const peakLatencyInput = () => {
+    return (
+      <h1>
+        PeakLatency Value
+      </h1>
+    )
+  }
+
   const peakLatencyOptions = {
     chart: {
       height: 250,
       type: "bar",
+      toolbar: {
+        show: true,
+        offsetX: -2,
+        offsetY: -25
+      }
     },
     plotOptions: {
       bar: {
@@ -62,18 +77,19 @@ const PeakLatencyChart = ({ data }) => {
         },
       },
     },
-    title: {
-      text: "Peak Latency > 500(ms)",
-      align: "middle",
-      offsetX: 0,
-      offsetY: 10,
-      style: {
-        color: colors.textColor[500],
-        fontSize: 16,
-        fontWeight: 500,
-        fontFamily: "Red Hat Display",
-      },
-    },
+    // title: {
+      // text: "Peak Latency > 500(ms)",
+      // align: "middle",
+      // offsetX: 0,
+      // offsetY: 10,
+      // style: {
+      //   color: colors.textColor[500],
+      //   fontSize: 16,
+      //   fontWeight: 500,
+      //   fontFamily: "Red Hat Display",
+      // },
+      
+    // },
     labels: {
       style: {
         colors: theme.palette.mode === "dark" ? "#FFF" : "#000",
@@ -91,18 +107,24 @@ const PeakLatencyChart = ({ data }) => {
   const chartWidth = isCollapsed ? 'calc(100% - 20px)' : 'calc(103% - 30px)'
 
   return (
-    <div data-theme={theme.palette.mode} style={{height:"calc(40vh - 20px)",width:chartWidth}} >
-      <ReactApexChart
-       style={{
-        color: "#000",
-      }}
-        options={peakLatencyOptions}
-        series={peakLatencySeries}
-        type="bar"
-        height={"90%"}
-        width={"100%"}
-      />
-    </div>
+    <>
+      <div className="chart-title">
+        <p>Peak Latency &gt; 500(ms)</p>
+      </div>
+
+      <div data-theme={theme.palette.mode} style={{ height: "calc(40vh - 20px)", width: chartWidth,marginTop:"-25px" }} >
+        <ReactApexChart
+          style={{
+            color: "#000",
+          }}
+          options={peakLatencyOptions}
+          series={peakLatencySeries}
+          type="bar"
+          height={"80%"}
+          width={"100%"}
+        />
+      </div>
+    </>
   );
 };
 
