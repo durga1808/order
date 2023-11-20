d0610cf3c66565378dd329edaf40282a200407dc
balanksp <a.bala101@gmail.com>
2023-11-20 17:40:15 +0530

    log summary responsive style

--- src/scenes/dashboard/summary/LogCharts/DebugBarChart.js
+++ src/scenes/dashboard/summary/LogCharts/DebugBarChart.js
@@ -4,11 +4,21 @@ import { useTheme } from "@emotion/react";
 import { tokens } from "../../../../theme";
 import { useContext } from "react";
 import { GlobalContext } from "../../../../global/globalContext/GlobalContext";
+import { Box, useMediaQuery } from "@mui/material";
 
 const DebugBarChart = ({ data }) => {
   const theme = useTheme();
   const colors = tokens(theme.palette.mode);
   const { isCollapsed } = useContext(GlobalContext);
+
+
+  const isSmallScreen = useMediaQuery((theme) => theme.breakpoints.down("sm"));
+  const isLandscape = useMediaQuery(
+    "(max-width: 1000px) and (orientation: landscape)"
+  );
+
+  const isiphone = useMediaQuery((theme) => theme.breakpoints.down("iphone"));
+
 
   const DebugBarChartOption = {
     chart: {
@@ -87,8 +97,21 @@ const DebugBarChart = ({ data }) => {
 
   const chartWidth = isCollapsed ? 'calc(100% - 10px)' : "calc(109% - 70px)"
 
+  const chartHeight = isLandscape && isSmallScreen ? "200%" : "90%";
+
   return (
-    <div data-theme={theme.palette.mode} style={{height: "calc(40vh - 20px)" ,width: chartWidth,}}>
+    <Box data-theme={theme.palette.mode} sx={{
+      // height: "calc(40vh - 20px)",
+      height:
+          isLandscape && isSmallScreen
+            ? "calc(45vh - 35px)"
+            : "calc(40vh - 20px)",
+        ...(isiphone && {
+          height: "calc(80vh - 32px)",
+
+          // backgroundColor: "grey",
+        }),
+      width: chartWidth,}}>
       <ReactApexChart
     
       style={{
@@ -97,10 +120,10 @@ const DebugBarChart = ({ data }) => {
         options={DebugBarChartOption}
         series={debugcountseries}
         type="bar"
-        height={"90%"}
+        height={chartHeight}
         width={"100%"}
       />
-    </div>
+    </Box>
   );
 };
 
--- src/scenes/dashboard/summary/LogCharts/WarnBarChart.js
+++ src/scenes/dashboard/summary/LogCharts/WarnBarChart.js
@@ -4,11 +4,19 @@ import { useTheme } from "@emotion/react";
 import { tokens } from "../../../../theme";
 import { useContext } from "react";
 import { GlobalContext } from "../../../../global/globalContext/GlobalContext";
+import { Box, useMediaQuery } from "@mui/material";
 
 const WarnBarChart = ({ data }) => {
   const theme = useTheme();
   const colors = tokens(theme.palette.mode);
   const { isCollapsed } = useContext(GlobalContext);
+
+  const isSmallScreen = useMediaQuery((theme) => theme.breakpoints.down("sm"));
+  const isLandscape = useMediaQuery(
+    "(max-width: 1000px) and (orientation: landscape)"
+  );
+
+  const isiphone = useMediaQuery((theme) => theme.breakpoints.down("iphone"));
 
   const WarnCountOptions = {
     chart: {
@@ -91,8 +99,21 @@ const WarnBarChart = ({ data }) => {
 
   const chartWidth = isCollapsed ? 'calc(100% - 10px)' : "calc(109% - 70px)"
 
+  const chartHeight = isLandscape && isSmallScreen ? "200%" : "90%";
+
   return (
-    <div data-theme={theme.palette.mode} style={{height: 'calc(40vh - 20px)' , width: chartWidth}}>
+    <Box data-theme={theme.palette.mode} style={{
+      // height: 'calc(40vh - 20px)' ,
+      height:
+          isLandscape && isSmallScreen
+            ? "calc(45vh - 35px)"
+            : "calc(40vh - 20px)",
+        ...(isiphone && {
+          height: "calc(80vh - 32px)",
+
+          // backgroundColor: "grey",
+        }),
+      width: chartWidth}}>
       <ReactApexChart
        style={{
         color: "#000",
@@ -100,10 +121,10 @@ const WarnBarChart = ({ data }) => {
         options={WarnCountOptions}
         series={warncountSeries}
         type="bar"
-        height={"90%"}
+        height={chartHeight}
         width={"100%"}
       />
-    </div>
+    </Box>
   );
 };
 
