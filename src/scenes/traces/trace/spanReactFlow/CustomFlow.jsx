6d4c8ff07d1c3d4c9f3c249deaa4a3d91c4da7bb
mariselvamchellaiya <mariselvamchellaiya@gmail.com>
2023-12-07 14:54:03 +0530

    kafak and function icon for custom flow

--- src/scenes/traces/trace/spanReactFlow/CustomFlow.jsx
+++ src/scenes/traces/trace/spanReactFlow/CustomFlow.jsx
@@ -1,45 +1,38 @@
 import { Card } from "@mui/material";
 import React from "react";
 import { useState } from "react";
-import './CustomFlow.css'
+import "./CustomFlow.css";
 import { MdHttp } from "react-icons/md";
 import { FaDatabase } from "react-icons/fa";
 import { SiApachekafka } from "react-icons/si";
+import { PiBracketsRoundBold } from "react-icons/pi";
 
 const CustomFlow = ({ spandata }) => {
-  const [spanName,setspanname]=useState([]);
+  const [spanName, setspanname] = useState([]);
 
   console.log("spandata", spandata);
-  console.log("spandnames",spanName);
+  console.log("spandnames", spanName);
 
-  const searchString='GET';
-
-
-
+  const searchString = "GET";
 
   // const spandata = [
   //   { spans: { name: "GET /orders/getOrders" } },
   //   { spans: { name: "SELECT * FROM orders" } },
   //   { spans: { name: "observability-demo-tables" } },
-    
+
   // ];
 
   const arr = ["GET", "SELECT", "ORDER", "PUT"];
 
   return (
     <div>
-      <Card sx={{ height: "calc(60vh - 32px)", padding: "10px" }}>
-      {spandata.map((span, index) => {
-        const spanName = span.spans.name
+      <Card sx={{ height: "calc(60vh - 32px)", padding: "10px",overflowY:"scroll" }}>
+        {spandata.map((span, index) => {
+          const spanName = span.spans.name;
 
-       
+          // Display Kafka icon only if both "thread.id" and "thread.name" are present
 
-
-         // Display Kafka icon only if both "thread.id" and "thread.name" are present
-          
           const attributes = span.spans.attributes || [];
-
-
 
           const hasThreadID = attributes.some(
             (attr) => attr.key === "thread.id"
@@ -47,51 +40,122 @@ const CustomFlow = ({ spandata }) => {
           const hasThreadName = attributes.some(
             (attr) => attr.key === "thread.name"
           );
-
-          const isKafka = hasThreadID && hasThreadName && attributes.length === 2;
-
-
-          const isHTTP = attributes.some(
-            (attr) => attr.key.includes("http.method")
+          const hasCodenamspace = attributes.some(
+            (attr) => attr.key === "code.namespace"
           );
-          // const isKafka = attributes.some(
-          //   (attr) => attr.key.includes
-          // );
-          const isDatabase = attributes.some(
-            (attr) => attr.key.includes("db")
+          const hasCodefunction = attributes.some(
+            (attr) => attr.key === "code.function"
           );
+          const hasCodeuser = attributes.some(
+            (attr) => attr.key === "user-id"
+          );
+
+          const isFunction =
+            hasThreadID && hasThreadName && attributes.length === 2 ||  hasThreadID &&
+            hasCodenamspace &&
+            hasCodefunction &&
+            hasThreadName &&
+            attributes.length === 4 ||  hasThreadID &&
+            hasCodenamspace &&
+            hasCodefunction &&
+            hasThreadName &&
+            hasCodeuser&&
+            attributes.length ===5||
+            hasCodeuser&&
+            hasThreadName &&
+            hasThreadID&&
+            attributes.length ===3
+
+
+
+          // const isFunction2 =
+          //   hasThreadID &&
+          //   hasCodenamspace &&
+          //   hasCodefunction &&
+          //   hasThreadName &&
+          //   attributes.length === 4;
+
+          const isHTTP = attributes.some((attr) =>
+            attr.key.includes("http.method")
+          );
+          const isKafka = attributes.some((attr) =>
+            attr.key.includes("message")
+          );
+          const isDatabase = attributes.some((attr) => attr.key.includes("db"));
 
           return (
-            <div key={index} className="circle" >
+            <div key={index} className="circle">
               {isHTTP && (
                 // Render HTTP icon
-                <MdHttp style={{ color: "white" ,backgroundColor:"gray",fontSize:"40px",padding:"8px",borderRadius:"50px"}} />
+                <MdHttp
+                  style={{
+                    color: "white",
+                    backgroundColor: "gray",
+                    fontSize: "40px",
+                    padding: "8px",
+                    borderRadius: "50px",
+                  }}
+                />
               )}
               {isDatabase && (
                 // Render Database icon
-                <FaDatabase style={{ color: "white" ,backgroundColor:"gray",fontSize:"40px",padding:"8px",borderRadius:"50px"}}  />
+                <FaDatabase
+                  style={{
+                    color: "white",
+                    backgroundColor: "gray",
+                    fontSize: "40px",
+                    padding: "8px",
+                    borderRadius: "50px",
+                  }}
+                />
               )}
-               {isKafka && (
+              {isKafka && (
                 // Render Database icon
-                <SiApachekafka style={{ color: "white" ,backgroundColor:"gray",fontSize:"40px",padding:"8px",borderRadius:"50px"}} />
+                <SiApachekafka
+                  style={{
+                    color: "white",
+                    backgroundColor: "gray",
+                    fontSize: "40px",
+                    padding: "8px",
+                    borderRadius: "50px",
+                  }}
+                />
               )}
+
+              {isFunction && (
+                // Render Database icon
+                <PiBracketsRoundBold
+                  style={{
+                    color: "white",
+                    backgroundColor: "gray",
+                    fontSize: "40px",
+                    padding: "8px",
+                    borderRadius: "50px",
+                  }}
+                />
+              )}
+
+              {/* {isFunction2 && (
+                // Render Database icon
+                <PiBracketsRoundBold
+                  style={{
+                    color: "white",
+                    backgroundColor: "gray",
+                    fontSize: "40px",
+                    padding: "8px",
+                    borderRadius: "50px",
+                  }}
+                />
+              )} */}
+
               {/* <p>{spanName}</p> */}
             </div>
-
-
-
           );
         })}
       </Card>
     </div>
   );
 };
-
-
-
- 
-
-
 
 //   return (
 //     <div>
@@ -100,12 +164,8 @@ const CustomFlow = ({ spandata }) => {
 //           // setspanname(span.spans.name)
 //           // setspanname(span.spans.name);
 
-          
 //         })}
 
-
-
-        
 //       </Card>
 //     </div>
 //   );
